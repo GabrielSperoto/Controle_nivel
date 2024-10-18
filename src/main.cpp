86a3cb4e5d1cb68c84f3c71209204013d55ea412
Gabriel Speroto <gabrielssperoto@gmail.com>
2024-10-18 07:53:59 -0300

    main para feature1

--- src/main.cpp
+++ src/main.cpp
@@ -18,8 +18,9 @@ float nivel_tanque = 0; // capacidade maxima do tanque
 float nivel_baixo = 0; // nivel minimo do tanque;
 float nivel_alto = 0; //nivel maximo do tanque
 bool flag;
-
-//xxxxx
+bool estado_bomba;
+bool shutdown;
+//teste 2
 
 UltraSonicDistanceSensor distanceSensor(TRIGGER,ECHO); //função para calcular a distância entre o nível da água e o sensor
 
@@ -41,6 +42,8 @@ void setup() {
 
 }
 void loop() {
+  //caso não haja parada total, execute o código normalmente
+  if (not shutdown){
 
     if(digitalRead(DESLIGA)){
       flag = false;
@@ -52,6 +55,7 @@ void loop() {
     //caso o nível da água esteja baixo e flag seja true ou o botão de ligar seja pressionado, ligar a bomba
     if((nivel < nivel_baixo) && (digitalRead(LIGA) || flag)){
       //liga o sistema quando o nível está baixo
+      estado_bomba = true;
       flag = true;
       digitalWrite(BOMBA,HIGH);
       digitalWrite(LED_VERMELHO,HIGH);
@@ -61,14 +65,12 @@ void loop() {
 
     else if((nivel >= nivel_alto) && (digitalRead(LIGA) || flag)){
       //desliga o sistema caso o nível da água esteja alto
+      estado_bomba = false;
+      flag = false;
       digitalWrite(BOMBA,LOW);
       digitalWrite(LED_VERDE,HIGH);
       digitalWrite(LED_VERMELHO,LOW);
       digitalWrite(LED_AMARELO,LOW);
-    }
-
-    else if(digitalRead(DESLIGA)){
-      flag = false;
     }
 
     else{
@@ -76,7 +78,36 @@ void loop() {
       digitalWrite(LED_VERDE,LOW);
       digitalWrite(LED_VERMELHO,LOW);
       digitalWrite(LED_AMARELO,LOW);
+
+    //parar tudo caso haja alguma anormalidade no funcionamento da bomba
+    if ((nivel < nivel_baixo && estado_bomba == false) || (nivel >= nivel_alto && estado_bomba == true)){ 
+      digitalWrite(BOMBA,LOW);
+      digitalWrite(LED_VERDE,LOW);
+      digitalWrite(LED_VERMELHO,LOW);
+      digitalWrite(LED_AMARELO,LOW);
+      shutdown = true;
+    }
      }
+      }
+  //No caso de shutdown == true, acenda o led amarelo e faça-o piscar
+  else{
+      digitalWrite(LED_AMARELO, HIGH);
+      delay(400);
+      digitalWrite(LED_AMARELO, LOW);
+      delay(400);
+      
+      //caso o botão de ligar seja acionado, shutdown será dado como false, reiniciando o sistema
+      if (digitalRead(LIGA)){
+        shutdown = false;
+      }
+      //caso o botão de desligar seja acionado, desligar todo o sistema de vez
+      else if (digitalRead(DESLIGA)){
+        digitalWrite(BOMBA,LOW);
+        digitalWrite(LED_VERDE,LOW);
+        digitalWrite(LED_VERMELHO,LOW);
+        digitalWrite(LED_AMARELO,LOW);
+      }
+    }
     
 
 
